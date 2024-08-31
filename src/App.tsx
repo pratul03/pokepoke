b6fbfc0588b023e9ddb817c031c62aed923e90f0
Pratul Makar <pratulmakar02@gmail.com>
2024-08-31 14:02:50 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,22 @@ import Footer from "./sections/Footer";
 import Navbar from "./sections/Navbar";
 import Wrapper from "./sections/Wrapper";
 import "./scss/index.scss";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="main-container">
       <BackGround />
-      <div className="app">
-        <Navbar />
-        <Wrapper />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route element={""} path="/search" />
+          </Routes>
+          <Wrapper />
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,14 +1,53 @@
 import React from 'react'
 import pokeballIcon from "../assets/pokeball-icon.png"
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+  const navigationRoutes = [
+    {
+      name: "Search",
+      route: "/search"
+    },
+    {
+      name: "Compare",
+      route: "/compare"
+    },
+    {
+      name: "Pokemon",
+      route: "/pokemon"
+    },
+    {
+      name: "List",
+      route: "/list"
+    },
+    {
+      name: "About",
+      route: "/about"
+    },
+  ]
   return (
     <nav>
       <div className="block">
         <img src={pokeballIcon} alt="pokeballIcon" />
       </div>
-      <div className="data"></div>
+      <div className="data">
+        <ul>
+          {
+            navigationRoutes.map(({ name, route }, index) => {
+              return (
+                <Link to={route} key={index}>
+                  <li key={name}>
+                    {
+                      name
+                    }
+                  </li>
+                </Link>
+              )
+            })
+          }
+        </ul>
+      </div>
       <div className="block">
         <GiHamburgerMenu />
       </div>
