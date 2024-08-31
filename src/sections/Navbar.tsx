5339c3ffd11bc12543e5ea8a431399f60eea0ea9
Pratul Makar <pratulmakar02@gmail.com>
2024-08-31 09:59:18 +0530

    update

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import pokeballIcon from "../assets/pokeball-icon.png"
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 export default function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav>
+      <div className="block">
+        <img src={pokeballIcon} alt="pokeballIcon" />
+      </div>
+      <div className="data"></div>
+      <div className="block">
+        <GiHamburgerMenu />
+      </div>
+    </nav>
   )
 }
