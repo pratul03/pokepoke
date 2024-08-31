aaf06cb8124bec64f55aba5bcc60bcfd36e60847
Pratul Makar <pratulmakar02@gmail.com>
2024-08-31 09:06:30 +0530

    added changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import BackGround from "./components/BackGround";
 import Footer from "./sections/Footer";
 import Navbar from "./sections/Navbar";
 import Wrapper from "./sections/Wrapper";
+import "./scss/index.scss";
+
 function App() {
   return (
     <div className="main-container">
