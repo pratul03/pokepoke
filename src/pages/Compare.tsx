2af384c5c69847191a9e491f30be0c4db9d44aa7
Pratul Makar <pratulmakar02@gmail.com>
2024-10-14 20:47:21 +0530

    on

--- src/pages/Compare.tsx
+++ src/pages/Compare.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import Wrapper from '../sections/Wrapper';
+import { useAppSelector } from "../app/hooks";
+import CompareContainer from "../components/CompareContainer";
+import Wrapper from "../sections/Wrapper";
 
 function Compare() {
+  const { compareQueue } = useAppSelector(({ pokemon }) => pokemon);
   return (
-    <div>
-      Compare
+    <div className="compare">
+      <CompareContainer
+        pokemon={compareQueue[0]}
+        isEmpty={compareQueue.length < 1}
+      />
+      <CompareContainer
+        pokemon={compareQueue[1]}
+        isEmpty={compareQueue.length < 2}
+      />
     </div>
-  )
+  );
 }
-export default  Wrapper(Compare);
+export default Wrapper(Compare);
