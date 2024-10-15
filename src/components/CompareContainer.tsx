e082c916049cb77d87c09adb6e08d7472dcf6eb7
Pratul Makar <pratulmakar02@gmail.com>
2024-10-15 21:11:46 +0530

    updated

--- src/components/CompareContainer.tsx
+++ src/components/CompareContainer.tsx
@@ -1,5 +1,25 @@
-const CompareContainer = () => {
-  return <div>CompareContainer</div>;
+import { FaPlus } from "react-icons/fa";
+import { usersPokemonsType } from "../utils/Types";
+
+const CompareContainer = ({
+  pokemon = undefined,
+  isEmpty = false,
+}: {
+  pokemon?: usersPokemonsType;
+  isEmpty?: boolean;
+}) => {
+  return (
+    <div className="compare-container">
+      {isEmpty && (
+        <div className="empty">
+          <button>
+            <FaPlus />
+          </button>
+          <h3>Add pokemon to Comparison</h3>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default CompareContainer;
